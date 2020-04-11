82fc2e0c073ed241d8fb4b6d53d0a665331d3592
Juan S. Valero <juanseval1@gmail.com>
2020-04-11 18:02:53 -0300

    Ejercicio columnas

--- test/test_celdas.py
+++ test/test_celdas.py
@@ -8,3 +8,14 @@ def test_contar_celdas():
             contador = contador + celda
 
     assert contador == 15
+
+def test_columnas():
+    carton1 = carton()
+    contador = 0
+    for x in range(9):
+        for j in range(3):
+            if carton1[j][x] == 1:
+                contador = contador + 1
+                break
+
+    assert contador == 9
